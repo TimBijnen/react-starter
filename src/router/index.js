d4c554bc727f042a343fdb91fa71305c7e6676d5
Tim Bijnen <tim.bijnen@login.greenhousegroup.com>
2019-03-07 14:25:16 +0100

    Add test

--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,18 @@ import P404 from "src/pages/404";
 
 import RestrictedRoute from "./restricted";
 
-const Router = ( { children, ...rest } ) => {
-    console.log(rest) 
-    return (
-        <BrowserRouter>
-            <React.Fragment>
-                { children }
-                <Switch>
-                    <RestrictedRoute exact path="/" { ...rest } component={ props => <Home { ...rest } { ...props } /> } />
-                    <Route exact path="/login" { ...rest } component={ props => <Login { ...rest } { ...props } /> } />
-                    <Route { ...rest } path="*" component={ P404 } />
-                </Switch>
-            </React.Fragment>
-        </BrowserRouter>
-    );
-}
+const Router = ( { children, ...rest } ) => (
+    <BrowserRouter>
+        <React.Fragment>
+            { children }
+            <Switch>
+                <RestrictedRoute exact path="/" { ...rest } component={ props => <Home { ...rest } { ...props } /> } />
+                <Route exact path="/login" { ...rest } component={ props => <Login { ...rest } { ...props } /> } />
+                <Route { ...rest } path="*" component={ P404 } />
+            </Switch>
+        </React.Fragment>
+    </BrowserRouter>
+);
 
 Router.propTypes = {
     children: PropTypes.node,
